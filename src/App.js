c764c555cdf16d94bfc9d6588e0f56b8c330813a
rasel <russelroy15@gmail.com>
2021-10-23 18:51:08 +0600

    WIP on load my data with node and express project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Found Users: {users.length}</h2>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" placeholder="your Name" />
+        <input type="submit" value="Submit" />
+      </form>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h3> Name: {user.name} </h3>
+          <h4>{user.born}</h4>
+        </div>
+      ))}
     </div>
   );
 }
